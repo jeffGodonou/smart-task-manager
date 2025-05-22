42ab20c0ee24624241579209b41bf15afdf39e34
jeffGodonou <jeff1949@hotmail.fr>
2025-05-22 16:10:41 -0400

    Set up of the JavaFX plugin to create a window for the application

--- src/main/java/com/jeff/taskmanager/App.java
+++ src/main/java/com/jeff/taskmanager/App.java
@@ -1,7 +1,19 @@
 package com.jeff.taskmanager;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Task Manager");
+        primaryStage.setScene(new Scene(new Label("Hello, welcome"), 400, 200));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
