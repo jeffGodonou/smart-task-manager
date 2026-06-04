80ec5cf2f316f4eb7f72d4d573a34799532fcff7
jeffGodonou <jeff1949@hotmail.fr>
2026-06-04 11:51:03 -0400

    Addition of Status to check the evolution of a task in the backend

--- src/main/java/com/jeff/taskmanager/model/Task.java
+++ src/main/java/com/jeff/taskmanager/model/Task.java
@@ -7,6 +7,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Entity
 @Table(name = "tasks")
 public class Task {
+    public enum Status {
+        TODO,
+        IN_PROGRESS,
+        DONE
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,6 +32,11 @@ public class Task {
         @JsonProperty("isCompleted")
     private boolean isCompleted;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    @JsonProperty("status")
+    private Status status;
+
     public Task() {}
 
     public Task(String title, String description, LocalDate dueDate, boolean isCompleted) {
@@ -34,6 +45,7 @@ public class Task {
         this.dueDate = dueDate;
         this.isCompleted = isCompleted;
         this.priority = "Medium"; // Default priority
+        this.status = Status.TODO;
     }
 
     public Long getId() {
@@ -82,5 +94,13 @@ public class Task {
 
     public void setPriority(String priority) {
         this.priority = priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
