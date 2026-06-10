1a90b7d523f38a0a68890ba444012860538b27d5
jeffGodonou <jeff1949@hotmail.fr>
2026-06-09 23:19:30 -0400

    Fix of import in the CalendarView component

--- frontend/frontend/src/components/CalendarView.tsx
+++ frontend/frontend/src/components/CalendarView.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Calendar, dateFnsLocalizer, Event as RbcEvent } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import enUS from 'date-fns/locale/en-US';
+import { Calendar, dateFnsLocalizer, type Event as RbcEvent } from 'react-big-calendar';
+import { format } from 'date-fns/format';
+import { parse } from 'date-fns/parse';
+import { startOfWeek }  from 'date-fns/startOfWeek';
+import { getDay } from 'date-fns/getDay';
+import { enUS } from 'date-fns/locale/en-US';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { listTasks, updateTask } from '../api/tasks';
