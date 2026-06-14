6227aaf8ab5ba0dac29b99a0fc0449befa234394
jeffGodonou <jeff1949@hotmail.fr>
2026-06-13 23:42:50 -0400

    Creation of User model and Repository

---
+++ src/main/java/com/jeff/taskmanager/model/User.java
@@ -0,0 +1,50 @@
+package com.jeff.taskmanager.model;
+
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public User() {}
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
