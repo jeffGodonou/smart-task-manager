0aa1c99f3062441501d29f938a1553efaa5d8394
jeffGodonou <jeff1949@hotmail.fr>
2026-06-13 23:40:49 -0400

    Integration of User entry in the backend API

--- src/main/java/com/jeff/taskmanager/api/BackendApplication.java
+++ src/main/java/com/jeff/taskmanager/api/BackendApplication.java
@@ -1,6 +1,8 @@
 package com.jeff.taskmanager.api;
 
+// import com.jeff.taskmanager.api.AuthController;
 import com.jeff.taskmanager.controler.TaskController;
+import com.jeff.taskmanager.repository.UserRepository;
 import com.jeff.taskmanager.service.TaskService;
 import com.sun.net.httpserver.HttpServer;
 
@@ -9,9 +11,15 @@ import java.net.InetSocketAddress;
 public class BackendApplication {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        TaskService taskService = new TaskService();
+        UserRepository userRepository = new UserRepository();
+        TaskService taskService = new TaskService(new com.jeff.taskmanager.repository.TaskRepository(), userRepository);
+
+        AuthController authController = new AuthController(userRepository);
+        authController.registerRoutes(server);
+
         TaskController controller = new TaskController(taskService);
         controller.registerRoutes(server);
+
         server.start();
         System.out.println("Backend API started at http://localhost:8080/api/tasks");
     }
